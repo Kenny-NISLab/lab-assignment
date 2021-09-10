21b6bab740f12ffb36a856432edb434a1157684b
Uzumal <kazuma.takeuchi.1027@gmail.com>
2021-09-10 12:43:17 +0900

    develop login

---
+++ pages/signin.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-form>
+    <v-container>
+      <v-text-field v-model="email" label="MailAddress"></v-text-field>
+      <v-text-field v-model="password" label="Password"></v-text-field>
+      <v-btn color="primary" @click="userSingIn">ログイン</v-btn>
+    </v-container>
+  </v-form>
+</template>
+<script>
+import firebase from '@/pages/firebase.js'
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    }
+  },
+  methods: {
+    userSingIn() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(() => {
+          alert('ログインしました。')
+        })
+        .catch(() => {
+          alert('メールアドレスかパスワードが間違っています。')
+        })
+    },
+  },
+}
+</script>
---
+++ pages/signout.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-form>
+    <v-container>
+      <h1 class="heading--main">ログアウト</h1>
+      <v-btn @click="singout"> ログアウト </v-btn>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+import firebase from '@/pages/firebase.js'
+export default {
+  methods: {
+    singout() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          alert('ログアウトしました。')
+          this.$router.push('/')
+        })
+        .catch((error) => {
+          alert(error)
+        })
+    },
+  },
+}
+</script>
